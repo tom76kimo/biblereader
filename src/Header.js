3d48c22853735bdad621d9cff2d76ab5f3d0de87
Tom Wu <tom76kimo@yahoo-inc.com>
2015-12-15 22:41:29 +0800

    add icon for google login button

--- src/Header.js
+++ src/Header.js
@@ -12,7 +12,11 @@ class RightBox extends Component {
   render() {
     return (
       <div className="Pos(a) End(0) T(0)">
-        <a href="/auth/google">Sign In with Google</a>
+        <a href="/auth/google">
+          <i className="fa fa-google Fz(30px) Mend(20px) C(white)">
+            <div className="Fz(14px) D(ib) Mstart(5px) Va(m)">Loggin</div>
+          </i>
+        </a>
       </div>
     );
   }
